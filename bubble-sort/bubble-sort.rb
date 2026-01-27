fa3585feaf035913c43e3f4842540fa016dda936
chrissven86 <christoffer.8618@gmail.com>
2026-01-27 14:12:29 +0100

    correction on return array-logic

--- bubble-sort/bubble-sort.rb
+++ bubble-sort/bubble-sort.rb
@@ -1,6 +1,6 @@
 def bubble_sort(array)
   array_length = array.size
-  return array if array_length < 1
+  return array if array_length <= 1
     
   loop do
     #swapped-variable to avoid infinite looping
